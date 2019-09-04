2ce76c81ebcab45699e7f0742e17920d310b7684
b.egorov <b.egorov@tinkoff.ru>
2019-09-04 15:55:43 +0300

    Chapter 3 ex 3

--- src/Chapter3.hs
+++ src/Chapter3.hs
@@ -1,12 +1,16 @@
 module Chapter3
     ( len
+    , mean
     ) where
+
+import Data.List
 
 --1. Write a function that computes the number of elements in a list. To test it, ensure that it gives the same answers as the standard length function.
 --2. Addatypesignatureforyourfunctiontoyoursourcefile.Totestit,loadthesource file into ghci again.
 len :: [a] -> Int
-len x = len' x 0
+len x = foldl' (const . succ) 0 x
 
-len' :: [a] -> Int -> Int
-len' [] l = l
-len' (h:t) l = len' t $! succ l
+--3. Write a function that computes the mean of a list, i.e., the sum of all elements in the list divided by its length.
+mean :: Fractional a => [a] -> a
+mean [] = 0
+mean x = let l = len x in foldl' (\s h -> s + h / fromIntegral l) 0 x
--- test/Spec.hs
+++ test/Spec.hs
@@ -9,3 +9,8 @@ main =  hspec $ do
       it "should work for a not empty list" $ do
         let xs = [42] in len xs `shouldBe` length xs
         let xs = ["a", "b", "c"] in len xs `shouldBe` length xs
+    describe "mean" $ do
+      it "should work for an empty list" $ mean [] `shouldBe` 0
+      it "should work for a not empty list" $ do
+        mean [42.73] `shouldBe` 42.73
+        mean [42.73, 73.42] `shouldBe` 58.075
